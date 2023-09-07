792d2688b09383a98731ed27086b6b343aba17ff
Marina <marinaakter878@gmail.com>
2023-09-07 12:47:29 +0600

    PHPlearning

---
+++ 1.data type.php
@@ -0,0 +1,22 @@
+<?php
+
+//integer and float
+var_dump(2.20);
+echo gettype(10);
+
+//string
+echo "hello world";
+
+//boolean
+echo true;
+echo false;
+
+//array
+$arr = array(1,2,3,4,5);
+print_r([1,2,3,4,5,6,7,8,9,10,"Hello",false]);
+
+
+
+
+
+
